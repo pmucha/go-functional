b0ee2ee88986a9d2795bfde93cb69f3a35268024
Paweł Mucha <pawel@mucha.com>
2022-01-24 16:48:08 +0100

    Create library base file

---
+++ functional/functional.go
@@ -0,0 +1,8 @@
+/*
+A library providing simple utilities helpful in everyday
+programming written in a functional manner --
+curried, iteratee-first data-last.
+
+All functions use generics, so they require Go ver. 1.18+.
+*/
+package functional
